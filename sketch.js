5f9379db09094280cdcdae7be617304d0b6fd14d
anikajkrishna <86082632+anikajkrishna@users.noreply.github.com>
2021-10-01 20:04:30 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,92 +1,114 @@
-var basket;
+var basket,basketImg;
 var blueBall;
 var purpleBall;
 var redBall;
 var yellowBall;
-var score
+var score;
+var red_ballImage;
+var blue_ballImage;
+var yellow_ballImage;
 
 function preload(){
-blueBall = loadImage("blue ball.png");
-purpleBall = loadImage("purple ball.png");
-redBall = loadImage("red ball.png");
-yellowBall = loadImage("yellow ball.png");
-basket = loadImage("basket.png");
+blue_ballImage = loadImage("blueBall.png");
+purple_ballImage = loadImage("purpleBall.png");
+red_ballImage = loadImage("redBall.png");
+yellow_ballImage = loadImage("yellowBall.png");
+basketImg = loadImage("basket.png");
 }
 
 function setup() {
-createCanvas(600, 200);
+createCanvas(600, 600);
 
-redBall= new Group();
-  redBall.add = ("red")
+redBallG= new Group();
+yellowBallG= new Group();
+blueBallG= new Group();
+purpleBallG= new Group();
+ 
+  basket = createSprite(300,550,10,10);
+  basket.addImage(basketImg);
+  
 
-yellowBall= new Group();
-  yellowBall.add = ("yellow")
-
-blueBall= new Group();
-  blueBall.add = ("blue")
-
-purpleBall= new Group();
-  purpleBall.add = ("purple")
-
-score = 0;
+  basket.scale = 0.2;
+  score = 0;
 }
 
 function redBall() {
-    var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
-    red.addImage(red_balloonImage);
+    var red = createSprite(Math.round(random(20, 570)),0, 10, 10);
+    red.addImage(red_ballImage);
     red.velocityY = 5;
     red.lifetime = 150;
-    red.scale = 0.1;
+    red.scale = 0.05;
+    redBallG.add(red)
   }
   
   function blueBall() {
-    var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
-    blue.addImage(blue_balloonImage);
+    var blue = createSprite(Math.round(random(20, 370)),0, 10, 10);
+    blue.addImage(blue_ballImage);
     blue.velocityY = 5;
     blue.lifetime = 150;
-    blue.scale = 0.1;
+    blue.scale = 0.05;
+    blueBallG.add(blue)
   }
   
   function yellowBall() {
-    var green = createSprite(0,Math.round(random(20, 370)), 10, 10);
-    green.addImage(green_balloonImage);
-    green.velocityY = 5;
-    green.lifetime = 150;
-    green.scale = 0.1;
+    var yellow = createSprite(Math.round(random(20, 370)),0, 10, 10);
+    yellow.addImage(yellow_ballImage);
+    yellow.velocityY = 5;
+    yellow.lifetime = 150;
+    yellow.scale = 0.05;
+    yellowBallG.add(yellow)
   }
   
   function purpleBall() {
-    var pink = createSprite(0,Math.round(random(20, 370)), 10, 10);
-    pink.addImage(pink_balloonImage);
-    pink.velocityY = 5;
-    pink.lifetime = 150;
-    pink.scale = 1;
+    var purple = createSprite(Math.round(random(20, 370)),0, 10, 10);
+    purple.addImage(purple_ballImage);
+    purple.velocityY = 5;
+    purple.lifetime = 150;
+    purple.scale = 0.05;
+    purpleBallG.add(purple)
   }
-
-function basket( ){
-    var basket = createSprite
-    basket.addImage(pink_balloonImage);
-    basket.velocityX = 5;
-    basket.lifetime = 150;
-    basket.scale = 1;
-
-}
 
 
 function draw() {
-if basket.isTouching (blueBall); {
-    score = score + 1
-}
-if basket.isTouching (yellowBall); {
-    score = score + 1
-}
-if basket.isTouching (purpleBall); {
-    score = score + 1
-}
-if basket.isTouching (redBall); {
-    score = score + 1
-}
+  background("white");
 
+  if (World.frameCount % 200 == 0) {
+    redBall();
+  }
+  if (World.frameCount % 170 == 0) {
+    blueBall();
+  }
+  if (World.frameCount % 105 == 0) {
+    yellowBall();
+  }
+  if (World.frameCount % 126 == 0) {
+    purpleBall();
+  }
+ 
+  basket.x = World. mouseX;
+
+  
+
+
+if (blueBallG.isTouching(basket)) {
+   score = score + 50
+   blueBallG.destroyEach();
+   
+}
+if (yellowBallG.isTouching(basket)) {
+    score = score + 100;
+    yellowBallG.destroyEach();
+}
+if (purpleBallG.isTouching(basket)) {
+    score = score + 75;
+    purpleBallG.destroyEach();
+}
+if (redBallG.isTouching(basket)) {
+    score = score + 25;
+    redBallG.destroyEach();
+} 
+    text("Score: "+ score, 500,50);
+    
     drawSprites();
 
 }
